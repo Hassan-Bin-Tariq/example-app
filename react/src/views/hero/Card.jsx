6a7b7ca25bf0b040729ef83405238ec54fb4b7de
Hassan Tariq <58106042+Hassan-Bin-Tariq@users.noreply.github.com>
2023-07-18 04:21:37 +0500

    card set

--- react/src/views/hero/Card.jsx
+++ react/src/views/hero/Card.jsx
@@ -1,25 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import './Card.css'
+import { BiCommentDetail } from 'react-icons/bi';
+import { BsFillBalloonHeartFill } from 'react-icons/bs';
+import { MdIosShare } from 'react-icons/md';
+import { BsFillFuelPumpFill } from 'react-icons/bs';
 
-const Card = ({ item: { id, cover, catgeory, title, authorName, time } }) => {
+const Card = ({ item }) => {
   return (
     <>
-      <div className='box'>
-        <div className='img'>
-          <img src={cover} alt='' />
-        </div>
-        <div className='text'>
-          <span className='category'>{catgeory}</span>
-          {/*<h1 className='titleBg'>{title}</h1>*/}
-          <Link to={`/SinglePage/${id}`}>
-            <h1 className='titleBg'>{title}</h1>
-          </Link>
-          <div className='author flex'>
-            <span>by {authorName}</span>
-            <span>{time}</span>
+      <div class="card">
+      <div class="imgBx">
+        <img src={item.urlToImage} alt="Card image"/>
+      </div>
+      <div class="content">
+        <div class="details">
+          <h2 className="title">{item.title}<br /><span className="auth">by {item.author}</span></h2>
+          <h5>{item.published_at.substring(0, 10)}<br /></h5>
+          <div class="data">
+            <h3><BsFillBalloonHeartFill size={15} color="red" /><br /><span>Like</span></h3>
+            <h3><BiCommentDetail size={15} color="red" /><br /><span>Comment</span></h3>
+            <h3><MdIosShare size={15} color="red" /><br /><span>Share</span></h3>
+          </div>
+          <div class="actionBtn">
+            <button>Read more</button>
           </div>
         </div>
       </div>
+    </div>
     </>
   )
 }
--- react/src/views/hero/Hero.jsx
+++ react/src/views/hero/Hero.jsx
@@ -6,10 +6,15 @@ import {useEffect} from "react";
 import axiosClient from "../../axios-client";
 import {useStateContext} from "../../contexts/ContextProvider";
 import axios from "axios";
+import { Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
+
 
 const Hero = () => {
   const [items, setIems] = useState(hero)
   const { setUser} = useStateContext();
+  const [nytimesArticles, setNytimesArticles] = useState([]);
+  const [guardianArticles, setguardianArticles] = useState([]);
+  const [newsArticles, setnewsArticles] = useState([]);
   useEffect(() => {
     axiosClient.get('/user')
       .then(({data}) => {
@@ -20,27 +25,62 @@ const Hero = () => {
           axios.post('http://localhost:8000/api/GetPreferedArticle', array) // Assuming your Laravel API endpoint for saving the user data is '/api/save-user'
           .then(response => {
             console.log(response.data);
+            setNytimesArticles(response.data['nytimesArticles']);
+            setguardianArticles(response.data['guardianArticles']);
+            setnewsArticles(response.data['newsArticles']);
           })
           .catch(error => {
             console.error(error);
           });
       })
   }, [])
+
+  const renderCardsInRows = () => {
+    const rows = [];
+    const cardsPerRow = 3;
+    const totalCards = 21;
+    let rowIndex = 0;
+  
+    while (rowIndex < totalCards) {
+      const rowItems = newsArticles
+        .slice(rowIndex, rowIndex + cardsPerRow)
+        .map((item) => {
+          // if (!item.urlToImage || !item.author) {
+          //   return null; // Skip the post if urlToImage or author is missing
+          // }
+  
+          return (
+            <TableCell key={item.id}>
+              <Card item={item} />
+            </TableCell>
+          );
+        });
+  
+      if (rowItems.some((item) => item !== null)) {
+        rows.push(<TableRow key={rowIndex}>{rowItems}</TableRow>);
+      }
+  
+      rowIndex += cardsPerRow;
+    }
+  
+    return rows;
+  };
+  
+  
+  
+  
   return (
-    <>
-      <section className='hero'>
-        <div className='container'>
-          {items.map((item) => {
-            return (
-              <>
-                <Card key={item.id} item={item} />
-              </>
-            )
-          })}
-        </div>
-      </section>
-    </>
-  )
-}
+    <section className="hero">
+      <TableContainer>
+        <Table>
+          <TableBody>
+            {renderCardsInRows()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </section>
+  );
+};
+
 
 export default Hero
